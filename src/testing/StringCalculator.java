6d51ab90470980ad67099ea0f159a5540cc177cf
szanwar29 <sanskar982265@gmail.com>
2022-09-08 02:38:00 +0530

    fixing multiple numbers

--- src/testing/StringCalculator.java
+++ src/testing/StringCalculator.java
@@ -1,6 +1,8 @@
 package testing;
 
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(String numbers) {
@@ -8,7 +10,21 @@ public class StringCalculator {
             return 0;
         } else {
             return Integer.parseInt(numbers);
+        int length = numbers.length();
+        if (length < 2) {
+            if (numbers.isEmpty()) {
+                return 0;
+            } else {
+                return convertToInt(numbers);
+            }
+        }else {
+            return Arrays.stream(numbers.split(","))
+                    .mapToInt(Integer::valueOf)
+                    .sum();
         }
     }
-}
 
+    public int convertToInt(String num){
+        return Integer.parseInt(num);
+    }
+}
