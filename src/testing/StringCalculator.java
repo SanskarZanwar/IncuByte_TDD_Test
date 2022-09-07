b979c03357445b86bb719498ff0bd2a479676dff
szanwar29 <sanskar982265@gmail.com>
2022-09-08 02:47:47 +0530

    adding remaining test cases

--- src/testing/StringCalculator.java
+++ src/testing/StringCalculator.java
@@ -1,30 +1,54 @@
 package testing;
 
 
-import java.util.Arrays;
-
 public class StringCalculator {
 
     public int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        } else {
-            return Integer.parseInt(numbers);
-        int length = numbers.length();
-        if (length < 2) {
+        if (numbers.length() < 2) {
             if (numbers.isEmpty()) {
                 return 0;
             } else {
                 return convertToInt(numbers);
             }
-        }else {
-            return Arrays.stream(numbers.split(","))
-                    .mapToInt(Integer::valueOf)
-                    .sum();
+        } else {
+            String delimiter = ",";
+            if (numbers.matches("//(.*)\n(.*)")) {
+                delimiter = Character.toString(numbers.charAt(2));
+                numbers = numbers.substring(4);
+            }
+
+            String[] numList = splitNumbers(numbers, delimiter + "|\n");
+            return sum(numList);
         }
     }
 
-    public int convertToInt(String num){
+    private String[] splitNumbers(String numbers, String divider) {
+        return numbers.split(divider);
+    }
+
+    private int convertToInt(String num) {
         return Integer.parseInt(num);
+    }
+
+    private int sum(String[] numbers) {
+        int total = 0;
+        StringBuilder negativeString = new StringBuilder();
+
+        for (String number : numbers) {
+            if (convertToInt(number) < 0) {
+                if (negativeString.toString().equals(""))
+                    negativeString = new StringBuilder(number);
+                else
+                    negativeString.append(",").append(number);
+            }
+            if (convertToInt(number) < 1000)
+                total += convertToInt(number);
+        }
+
+        if (!negativeString.toString().equals("")) {
+            throw new IllegalArgumentException("Negatives not allowed: " + negativeString);
+        }
+
+        return total;
     }
 }
--- src/testing/StringCalculatorTest.java
+++ src/testing/StringCalculatorTest.java
@@ -2,21 +2,65 @@ package testing;
 
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
-
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 
 public class StringCalculatorTest {
-    @Test
-    public void testAdd() {
-        StringCalculator simpleCalculator = new StringCalculator();
-        assertEquals(0, simpleCalculator.add(""));
-        assertEquals(1, simpleCalculator.add("1"));
-        assertEquals(2, simpleCalculator.add("2"));
+
+    StringCalculator stringCalculator;
+
+    @BeforeTest
+    public void init() {
+        stringCalculator = new StringCalculator();
     }
+
+    @AfterTest
+    public void destroy() {
+        stringCalculator = null;
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    public void testAddOneNumber() {
+        assertEquals(1, stringCalculator.add("1"));
+    }
+
     @Test
     public void testAddMultipleNumbers() {
-        StringCalculator simpleCalculator = new StringCalculator();
-        assertEquals(3, simpleCalculator.add("1,2"));
-        assertEquals(25, simpleCalculator.add("13,12"));
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
+
+    @Test
+    public void testNewLine() {
+        assertEquals(10, stringCalculator.add("1\n2,3\n4"));
+    }
+
+    @Test
+    public void testNegativeNumber() {
+        try {
+            stringCalculator.add("-1,2");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "Negatives not allowed: -1");
+        }
+        try {
+            stringCalculator.add("1,-2,3,-5");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "Negatives not allowed: -2,-5");
+        }
+    }
+
+    @Test
+    public void testOverThousand() {
+        assertEquals(10, stringCalculator.add("1000,10"));
+    }
+
+    @Test
+    public void testOtherDelimiter() {
+        assertEquals(3, stringCalculator.add("//;\n1;2"));
     }
 
 }
